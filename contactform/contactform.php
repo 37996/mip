5d7294924f9b000b3ad9358cdc56fc22d3399bc6
Manoel Figueiredo <figueiredoufam@gmail.com>
2019-07-15 20:11:59 +0100

    formulario3

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,29 +1,43 @@
 <?php
-function sendMail($de,$para,$mensagem,$assunto)
-{
-    // require_once('phpmailer/class.phpmailer.php');
-    require_once('contactform/contactform.php');
-    $mail = new PHPMailer(true);
-    $mail->IsSMTP();
-    try {
-      $mail->SMTPAuth   = true;
-      $mail->Host       = 'smtp.gmail.com';
-      $mail->SMTPSecure = "tls"; #remova se nao usar gmail
-     $mail->Port       = 587;                  #remova se nao usar gmail
-      $mail->Username   = 'contatomipbr@gmail.com';
-      $mail->Password   = '08080403mip';
-      $mail->AddAddress($para);
-     $mail->AddReplyTo($de);
-     $mail->SetFrom($de);
-      $mail->Subject = $assunto;
-      $mail->MsgHTML($mensagem);
-      $mail->Send();    
-      $envio = true;
-    } catch (phpmailerException $e) {
-      $envio = false;
-    } catch (Exception $e) {
-      $envio = false;
-    }
-    return $envio;
+
+// Email address verification
+function isEmail($email) {
+	return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
+
+if($_POST) {
+
+    // Enter the email where you want to receive the message
+    $emailTo = 'contatomipbr@gmail.com';
+
+    $name = addslashes(trim($_POST['name']));
+    $clientEmail = addslashes(trim($_POST['email']));
+    $subject = addslashes(trim($_POST['subject']));
+    $message = addslashes(trim($_POST['message']));
+
+    $array = array('nameMessage' => '', 'emailMessage' => '', 'subjectMessage' => '', 'messageMessage' => '');
+
+    if($name == '') {
+    	$array['nameMessage'] = 'Empty name!';
+    }
+    if(!isEmail($clientEmail)) {
+        $array['emailMessage'] = 'Invalid email!';
+    }
+    if($subject == '') {
+        $array['subjectMessage'] = 'Empty subject!';
+    }
+    if($message == '') {
+        $array['messageMessage'] = 'Empty message!';
+    }
+    if($name != '' && isEmail($clientEmail) && $subject != '' && $message != '') {		
+		// Send email
+		$message = "Message from: " . $name . "\r\n" . $message;
+		$headers = "From: " . $clientEmail . " <" . $clientEmail . ">" . "\r\n" . "Reply-To: " . $clientEmail;
+		mail($emailTo, $subject . " (contact form template)", $message, $headers);
+    }
+
+    echo json_encode($array);
+
+}
+
 ?>
