8a4aa91585f8bc1461c1cb6ee62263661eaf049c
Manoel Figueiredo <figueiredoufam@gmail.com>
2019-07-15 20:18:24 +0100

    formulario4

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,43 +1,67 @@
 <?php
 
-// Email address verification
-function isEmail($email) {
-	return filter_var($email, FILTER_VALIDATE_EMAIL);
+$errorMSG = "";
+
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
 }
 
-if($_POST) {
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
 
-    // Enter the email where you want to receive the message
-    $emailTo = 'contatomipbr@gmail.com';
+// MSG SUBJECT
+if (empty($_POST["msg_subject"])) {
+    $errorMSG .= "Subject is required ";
+} else {
+    $msg_subject = $_POST["msg_subject"];
+}
 
-    $name = addslashes(trim($_POST['name']));
-    $clientEmail = addslashes(trim($_POST['email']));
-    $subject = addslashes(trim($_POST['subject']));
-    $message = addslashes(trim($_POST['message']));
 
-    $array = array('nameMessage' => '', 'emailMessage' => '', 'subjectMessage' => '', 'messageMessage' => '');
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
 
-    if($name == '') {
-    	$array['nameMessage'] = 'Empty name!';
+//Add your email here
+$EmailTo = "contatomipbr@gmail.com";
+$Subject = "New Message Received";
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Subject: ";
+$Body .= $msg_subject;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
     }
-    if(!isEmail($clientEmail)) {
-        $array['emailMessage'] = 'Invalid email!';
-    }
-    if($subject == '') {
-        $array['subjectMessage'] = 'Empty subject!';
-    }
-    if($message == '') {
-        $array['messageMessage'] = 'Empty message!';
-    }
-    if($name != '' && isEmail($clientEmail) && $subject != '' && $message != '') {		
-		// Send email
-		$message = "Message from: " . $name . "\r\n" . $message;
-		$headers = "From: " . $clientEmail . " <" . $clientEmail . ">" . "\r\n" . "Reply-To: " . $clientEmail;
-		mail($emailTo, $subject . " (contact form template)", $message, $headers);
-    }
-
-    echo json_encode($array);
-
 }
 
 ?>
